15d1dca504ae1613e1e82974e8e484dacd7ed62c
Senthil Ramalingam <racksen@hotmail.com>
2017-12-08 18:53:01 -0800

    P2-Answers

--- projects/finding_donors/visuals.py
+++ projects/finding_donors/visuals.py
@@ -65,18 +65,18 @@ def evaluate(results, accuracy, f1):
     # Constants
     bar_width = 0.3
     colors = ['#A00000','#00A0A0','#00A000']
-    
+
     # Super loop to plot four panels of data
     for k, learner in enumerate(results.keys()):
         for j, metric in enumerate(['train_time', 'acc_train', 'f_train', 'pred_time', 'acc_test', 'f_test']):
             for i in np.arange(3):
-                
                 # Creative plot code
-                ax[j/3, j%3].bar(i+k*bar_width, results[learner][i][metric], width = bar_width, color = colors[k])
-                ax[j/3, j%3].set_xticks([0.45, 1.45, 2.45])
-                ax[j/3, j%3].set_xticklabels(["1%", "10%", "100%"])
-                ax[j/3, j%3].set_xlabel("Training Set Size")
-                ax[j/3, j%3].set_xlim((-0.1, 3.0))
+                tempIndex= j//3 
+                ax[tempIndex, j%3].bar(i+k*bar_width, results[learner][i][metric], width = bar_width, color = colors[k])
+                ax[tempIndex, j%3].set_xticks([0.45, 1.45, 2.45])
+                ax[tempIndex, j%3].set_xticklabels(["1%", "10%", "100%"])
+                ax[tempIndex, j%3].set_xlabel("Training Set Size")
+                ax[tempIndex, j%3].set_xlim((-0.1, 3.0))
     
     # Add unique y-labels
     ax[0, 0].set_ylabel("Time (in seconds)")
